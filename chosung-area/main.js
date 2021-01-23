1df3564dd14e2b751d214bd905cc9e5a52d6f434
myyh1234 <ccw040607@gmail.com>
2021-01-24 02:02:13 +0900

    add table

--- chosung-area/main.js
+++ chosung-area/main.js
@@ -1,4 +1,9 @@
 var board_size, player_cnt;
+var chosung = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+function choose(arr){
+    var idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+}
 
 function parse(){
     var tmp = location.href.toString().split('?')[1].split('&');
@@ -14,4 +19,25 @@ function parse(){
         player_cnt = 4;
     
     console.log(board_size, player_cnt);
+}
+
+
+
+function build_board(parent){
+    for (var i = 0; i < board_size; i++){
+        var now = document.createElement('div');
+        now.id = 'row ' + i;
+        now.setAttribute('class', 'row');
+        parent.appendChild(now);
+        for (var j = 0; j < board_size; j++){
+            var newcell = document.createElement('div');
+            newcell.id = 'cell ' + i + '-' + j;
+            newcell.setAttribute('class', 'cell')
+            newcell.style.width = Math.floor(450 / board_size).toString() + 'px'; 
+            newcell.style.height = Math.floor(450 / board_size).toString() + 'px';
+            newcell.innerHTML = '<span id="' + i + ' ' + j + '">' + choose(chosung) + '</span>';
+            now.appendChild(newcell);
+        }
+
+    }
 }
