c15c9df23853e1ff50a72c63001e3e2f59ab804b
myyh1234 <ccw040607@gmail.com>
2021-01-25 03:30:44 +0900

    changed api key

--- chosung-area/check_web.js
+++ chosung-area/check_web.js
@@ -1,5 +1,5 @@
 function check_word(word){
-    var key = '05D35A13E73CE4BB252E84AF5A5B47A0';
+    var key = '79DFD4B08A0FF12EE31D1968FFF0002B';
     var uri_url = 'https://opendict.korean.go.kr/api/search?key=' + key + '&q=' + word + '&advanced=y&pos=1,2,3';
     var uri_sam = fetch(uri_url);
     console.log(uri_sam);
