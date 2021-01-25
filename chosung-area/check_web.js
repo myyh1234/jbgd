3237239fe51ab507bd2c0a7ae5b61cdacc73d1f2
myyh1234 <ccw040607@gmail.com>
2021-01-25 18:37:14 +0900

    calcel using api

--- chosung-area/check_web.js
+++ chosung-area/check_web.js
@@ -1,7 +1,4 @@
 function check_word(word){
-    var key = '79DFD4B08A0FF12EE31D1968FFF0002B';
-    var uri_url = 'https://opendict.korean.go.kr/api/search?key=' + key + '&q=' + word + '&advanced=y&pos=1,2,3';
-    var uri_sam = fetch(uri_url);
-    console.log(uri_sam);
+    // 사전 api, 크롤링 등으로 있는 단어인지 체크하기
     return true;
 }
